00dd1474161bad3b1caa78346cab3ee799984f4d
Asipko <avsipko@gmail.com>
2022-11-13 14:32:46 +0300

    nav add

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,19 @@
 import React from "react"
-import img22 from '../image/image 22.png'
-import img23 from '../image/image 23.png'
-import img24 from '../image/image 24.png'
-import img25 from '../image/image 25.png'
-import img26 from '../image/image 26.png'
-import img27 from '../image/image 27.png'
-import img28 from '../image/image 28.png'
-import img29 from '../image/image 29.png'
-import img30 from '../image/image 30.png'
+import group from '../image/Group 77.png'
+import '../css/Main.css';
 
 
 export default function Main() {
+
+    const link = "https://www.youtube.com/watch?v=bMknfKXIFA8"
     return (
-        <main>
-            <h1>Generac Compnent</h1>
-            <img className="nav--logo" src={img22} alt="logo"></img>
-            <img className="nav--logo" src={img23} alt="logo"></img>
-            <img className="nav--logo" src={img24} alt="logo"></img>
-            <img className="nav--logo" src={img25} alt="logo"></img>
-            <img className="nav--logo" src={img26} alt="logo"></img>
-            <img className="nav--logo" src={img27} alt="logo"></img>
-            <img className="nav--logo" src={img28} alt="logo"></img>
-            <img className="nav--logo" src={img29} alt="logo"></img>
-            <img className="nav--logo" src={img30} alt="logo"></img>
-        </main>
+        <section className="hero">
+            <img className="hero--foto" src={group} alt="hero"></img>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activitiesled by one-of-a-kind hosts—all without leaving home.
+</p>
+    <p>2:44 to {link}</p>
+
+        </section>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react"
 import '../css/Navbar.css';
 import logorairbnb from '../image/airbnb-logo.png'
 
+
 export default function Navbar() {
     return (<nav className="nav">
                <img className="nav--logo" src={logorairbnb} alt="logo"></img>
