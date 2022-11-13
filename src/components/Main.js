7924956253dab0895ba515ad1ebbc80222556289
Asipko <avsipko@gmail.com>
2022-11-13 14:07:21 +0300

    add image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,28 @@
 import React from "react"
+import img22 from '../image/image 22.png'
+import img23 from '../image/image 23.png'
+import img24 from '../image/image 24.png'
+import img25 from '../image/image 25.png'
+import img26 from '../image/image 26.png'
+import img27 from '../image/image 27.png'
+import img28 from '../image/image 28.png'
+import img29 from '../image/image 29.png'
+import img30 from '../image/image 30.png'
+
 
 export default function Main() {
     return (
         <main>
             <h1>Generac Compnent</h1>
+            <img className="nav--logo" src={img22} alt="logo"></img>
+            <img className="nav--logo" src={img23} alt="logo"></img>
+            <img className="nav--logo" src={img24} alt="logo"></img>
+            <img className="nav--logo" src={img25} alt="logo"></img>
+            <img className="nav--logo" src={img26} alt="logo"></img>
+            <img className="nav--logo" src={img27} alt="logo"></img>
+            <img className="nav--logo" src={img28} alt="logo"></img>
+            <img className="nav--logo" src={img29} alt="logo"></img>
+            <img className="nav--logo" src={img30} alt="logo"></img>
         </main>
     )
 }
