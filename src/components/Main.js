883f878bb8b268f8dab64ff0e014f7fb768aec97
Asipko <avsipko@gmail.com>
2022-11-13 13:06:35 +0300

    new init

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,9 @@
 import React from "react"
-import '../css/Main.css';
 
 export default function Main() {
     return (
         <main>
-            <h1 className="main--titel">Fun about React</h1>
-            <ul className="main--facts">
-                <li>First  and second comand that do you need</li>
-                <li>First 1 and comand that do you need</li>
-                <li>First 2  and second that do you </li>
-                <li>First 3  and second comand do you need</li>
-                <li>First 4  and second comand</li>
-                <li>First 5  and second comand that do you need</li>
-            </ul>
+            <h1>Generac Compnent</h1>
         </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from "react"
 import '../css/Navbar.css';
-import ReactLogo from "../image/Unknown2.webp";
+
 
 export default function Nacbar() {
     return (<nav className="nav">
-                <img src={ReactLogo} alt="react_img" />
                 <ul>
                     <li>React Course</li>
                     <li>AvSipko</li>
