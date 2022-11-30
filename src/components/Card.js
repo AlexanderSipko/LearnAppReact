4b6dcf67368204aab994692ec36dd2a9db60088f
Asipko <avsipko@gmail.com>
2022-11-30 21:25:34 +0500

    status

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,17 @@ export default function Card(props) {
     const imagesrc = require('../image/Star 1.png');
     const img = require(`../image/${props.img}`);
 
+    let badgetText
+    if (props.openSpots === 0) {
+        badgetText = 'SOLD OUT'
+    } else if (props.location === 'Online') {
+        badgetText = 'ONLINE'
+    } 
+
     return (
         <div className="card">
+            
+            {badgetText && <div className="card--badge">{badgetText}</div>}
             <img src={img} className="card--image" alt=''/>
             <div className="card--stats">
                 <img src={imagesrc} className="card--star" alt=''/>
