3dd550e0519ee18ab7d246cb9585af7ceddbd1ae
Asipko <avsipko@gmail.com>
2022-12-01 18:49:02 +0500

    predict

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Card from './components/Card';
 import './css/App.css';
 import data from './components/data';
 
+
+import {predict_number, getYourNumber} from './components/function'
+
+
 export default function App() {
+
+
 
   const cards = data.map(item => {
     return (
@@ -16,13 +22,33 @@ export default function App() {
       )
   });
 
+  function handlerChang() {
+    let text = document.querySelector('#date--1').value
+    let state
+    let number = getYourNumber(text)
+    state = predict_number[number]
+    document.querySelector('.predict--text').textContent = state
+    document.querySelector('.predict--date').textContent =  `Вы указали дату рождения: ${text}`
+    document.querySelector('.predict--number').textContent =  `Ваша цифра: ${number}`
+  }
+
   return (
     <div>
       <Navbar />
-      <Main />
-        <section className='cards-list'>
+      {/* <Main /> */}
+
+          <section className='predict--section'>
+            <h1>Предсказание по дате рождения</h1>
+            <input id='date--1'></input>
+            <button onClick={handlerChang}>Predict</button>
+            <p></p>
+            <h1 className='predict--date'></h1>
+            <p className='predict--number'></p>
+            <h2 className='predict--text'> </h2>
+          </section>
+          {/* <section className='cards-list'>
           {cards}
-        </section>
+        </section> */}
       <Footer />
     </div>
   );
